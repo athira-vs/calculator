e9c36e93e24248f520790072600f4bffe063cdc4
athira <vsathira33@gmail.com>
2021-09-06 16:14:28 +0530

    Modified main.py to take operation as command lineargument.
    - Removed the user interaction option
    - The operation is to be passed as command line argument now.

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 import sys
 from calc import Calc
 
-while True:
-    print("______MENU______")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-    print("5. Floor Division")
-    print("6. Modulo Division")
-    print("7. Exit")
+print("Command: Perform multiplication, 10*20")
+print("Command Format: python3 main.py 3 10 20")
 
-    ch = input("Enter your choice: ")
+#while True:
+print("______MENU______")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Floor Division")
+print("6. Modulo Division")
+print("7. Exit")
 
-    if ch and ch != '7':
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        c = Calc(a, b)
+ch = sys.argv[1]
+
+if ch and ch != '7':
+    a = int(sys.argv[2])
+    b = int(sys.argv[3])
+    c = Calc(a, b)
 
 
 
-    if ch == '1':
-        print(f"{a} + {b} = {c.add()}")
-    elif ch == '2':
-        print(f"{a} - {b} = {c.subtract()}")
-    elif ch == '3':
-        print(f"{a} * {b} = {c.multiply()}")
-    elif ch == '4':
-        print(f"{a} / {b} = {c.divide()}")
-    elif ch == '5':
-        print(f"{a} // {b} = {c.floor_divide()}")
-    elif ch == '6':
-        print(f"{a} % {b} = {c.modulo_divide()}")
-    elif ch == '7':
-        break
-    else:
-        print("Invalid Entry!!!")
+if ch == '1':
+    print(f"{a} + {b} = {c.add()}")
+elif ch == '2':
+    print(f"{a} - {b} = {c.subtract()}")
+elif ch == '3':
+    print(f"{a} * {b} = {c.multiply()}")
+elif ch == '4':
+    print(f"{a} / {b} = {c.divide()}")
+elif ch == '5':
+    print(f"{a} // {b} = {c.floor_divide()}")
+elif ch == '6':
+    print(f"{a} % {b} = {c.modulo_divide()}")
+#elif ch == '7':
+    #break
+else:
+    print("Invalid Entry!!!")
